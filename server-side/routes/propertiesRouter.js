df76b11c97b9d287ecaab0f9177f7e01001005e5
Chavi <chavideveloper@gmail.com>
2024-06-18 08:58:10 +0300

    start add properties to server

---
+++ server-side/routes/propertiesRouter.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const router = express.Router();
+const properties=require('../services/properties');
+
+router.get('/sectors',async function (req, res,next){
+    
+    try {
+        let sectors= await properties.getSectors();
+        res.json(sectors);
+      }
+       catch (err) {
+        console.log(err);
+        next(err);
+      }
+    
+});
+
+
+router.get('/genders',async function (req, res,next){
+    
+    try {
+        let genders= await properties.getGenders();
+        res.json(genders);
+      }
+       catch (err) {
+        console.log(err);
+        next(err);
+      }
+    
+});
+
+
+
+router.get('/areas',async function (req, res,next){
+    
+    try {
+        let areas= await properties.getAreas();
+        res.json(areas);
+      }
+       catch (err) {
+        console.log(err);
+        next(err);
+      }
+    
+});
+
+
+
+module.exports = router;
