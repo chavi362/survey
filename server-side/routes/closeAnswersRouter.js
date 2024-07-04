272f666a2b26265e587e43f1e9a4875e5d3faadb
Chavi <chavideveloper@gmail.com>
2024-07-04 16:22:10 +0300

    authentication to answers

--- server-side/routes/closeAnswersRouter.js
+++ server-side/routes/closeAnswersRouter.js
@@ -3,7 +3,11 @@ const answersRouter = express.Router({ mergeParams: true });
 const controller = require("../controllers/closeAnswersController");
 const authenticateToken = require('../middlewares/authenticateToken');
 const surveyMiddleware = require("../middlewares/surveyMiddleware");
+const checkSurveyManager = require('../middlewares/checkSurveyManager');
+
 answersRouter.use(surveyMiddleware);
-answersRouter.post("/", authenticateToken, controller.createAnswer);
-answersRouter.put("/:answerCode", authenticateToken, controller.updateAnswer);
+answersRouter.post("/", authenticateToken, checkSurveyManager, controller.createAnswer);
+answersRouter.put("/:answerCode", authenticateToken, checkSurveyManager, controller.updateAnswer);
+answersRouter.delete("/:answerCode", authenticateToken, checkSurveyManager, controller.deleteAnswer);
+
 module.exports = answersRouter;
