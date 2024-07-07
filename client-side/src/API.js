6c80290498c53abe8352dc0b2c8ab10da3bcdeb0
Chavi <chavideveloper@gmail.com>
2024-07-07 22:33:24 +0300

    sitting with teacher

--- client-side/src/API.js
+++ client-side/src/API.js
@@ -2,6 +2,8 @@ import Cookies from 'js-cookie';
 
 export async function serverRequests(method, URL, body) {
     const token = Cookies.get('token');
+    console.log(Cookies)
+    console.log(token)
     const headers = {
         'Content-Type': 'application/json',
         Authorization: `Bearer ${token}`,
@@ -13,7 +15,7 @@ export async function serverRequests(method, URL, body) {
 
     if (method === 'GET') {
         try {
-            const fetchResponse = await fetch(`http://localhost:3000/${URL}`, { method: 'GET', headers });
+            const fetchResponse = await fetch(`http://localhost:3000/${URL}`, { method: 'GET', headers, credentials: 'include' });
             if (fetchResponse.ok) {
                 console.log('Fetch response headers:', fetchResponse.headers);
                 return fetchResponse;
